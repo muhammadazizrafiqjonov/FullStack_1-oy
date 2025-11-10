4d25a9aef76a11620887ac11ddae5537ac9c02e8
Muhammadaziz <muhammadazizrafiqjonov1@gmail.com>
2025-11-10 18:08:15 +0500

    1-dars

---
+++ 1-dars/index.js
@@ -0,0 +1,35 @@
+//FIZZBUZZ Misoli
+
+
+// let a = 15
+
+// if (a % 3 == 0 && a % 5 == 0){
+//     console.log("FIZZBUZZ")
+// }
+// if (a % 3 == 0 && a % 5 != 0){
+//     console.log("FIZZ")
+// }
+// if (a % 3 != 0 && a % 5 == 0){
+//     console.log("BUZZ")
+// }
+// else {
+//     console.log("")
+// }
+
+
+
+let n = 33
+
+if (n > 0){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if (n % 2 == 0){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
