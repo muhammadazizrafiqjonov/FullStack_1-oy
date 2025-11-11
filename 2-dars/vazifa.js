4963a02e203b8fd9739711bfd0c7f23ef3805e0b
Muhammadaziz <muhammadazizrafiqjonov1@gmail.com>
2025-11-12 03:59:33 +0500

    2-dars

---
+++ 2-dars/vazifa.js
@@ -0,0 +1,129 @@
+//  1 -masala
+
+// let son1 = 12 , son2 = -15, son3 = 50
+
+// if (son1 < 0) {
+//     console.log(son1)
+// }
+// if (son2 < 0) {
+//     console.log(son2)
+// }
+// if (son3 < 0) {
+//     console.log(son3)
+// }
+
+// 2 -masala
+
+
+// let a = 15
+
+// if (a % 3 == 0 && a % 5 == 0){
+//     console.log("FIZZBUZZ")
+// }
+// if (a % 3 == 0 && a % 5 != 0){
+//     console.log("FIZZ")
+// }
+// if (a % 3 != 0 && a % 5 == 0){
+//     console.log("BUZZ")
+// }
+// else {
+//     console.log("")
+// }
+
+
+//  3-masala
+
+
+// let son1 = 19, son2 = 15, son3 = 50, max, min, mid
+
+// if (son1 > son2 && son1 > son3) {
+//     max = son1
+//     console.log("Eng katta son: ", max)
+// }
+// else if (son2 > son3) {
+//     max = son2
+//     console.log("Eng katta son: ", max)
+// }
+// else {
+//     max = son3
+//     console.log("Eng katta son: ", max)
+// }
+
+// if (son1 < son2 && son1 < son3) {
+//     min = son1
+//     console.log("Eng kichik son: ", min)
+// }
+// else if (son2 < son3) {
+//     min = son2
+//     console.log("Eng kichik son: ", min)
+// }
+// else {
+//     min = son3
+//     console.log("Eng kichik son: ", min)
+// }
+
+// mid = son1 + son2 + son3 - max - min
+// console.log("Sonlarning o'rtachasi : ", mid)
+
+
+//  4 - masala
+
+
+// let son = 16
+
+// if (son % 3 == 0) {
+//     console.log("3")
+//     if (son % 5 == 0){
+//         console.log("5")
+//         if (son % 7 == 0){
+//             console.log("7")
+//         }
+//     }
+// }
+// if (son % 3 != 0 && son % 5 != 0 && son % 7 != 0) {
+//     console.log("-1")
+// }
+
+
+// 5 - masala
+
+
+// let R = {
+//     A : 13,
+//     U : 5,
+//     O : 1,
+//     N : "R-jamoasi"
+// }
+// let B = {
+//     A : 14,
+//     U : 4,
+//     O : 1,
+//     N : "B-jamoasi"
+// }
+
+// if (R.A > B.A) {
+//     console.log("G'olib jamoa :", R.N)
+// }
+// else if (R.A < B.A) {
+//     console.log("G'olib jamoa :", B.N)
+// }
+// else {
+//     if (R.U > B.U) {
+//         console.log("G'olib jamoa :", R.N)
+//     }
+//     else if (R.U < B.U) {
+//         console.log("G'olib jamoa :", B.N)
+//     }
+//     else {
+//         if (R.O > B.O) {
+//             console.log("G'olib jamoa :", R.N)
+//         }
+//         else if (R.O < B.O) {
+//             console.log("G'olib jamoa :", B.N)
+//         }
+//         else {
+//             console.log("Durrang")
+//         }
+
+//     }
+// }
