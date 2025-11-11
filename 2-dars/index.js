054e3fd7fde8655024ccd63277e6608eeb2766cb
Muhammadaziz <muhammadazizrafiqjonov1@gmail.com>
2025-11-11 13:16:29 +0500

    2-dars

--- 2-dars/index.js
+++ 2-dars/index.js
@@ -1,3 +1,158 @@
-let a =25
+// let a =25
 
-console.log(a)
+// console.log(a)
+
+// let a = 23
+// let b = 30
+
+// a = a + b
+// b = a - b
+// a = a - b
+
+// console.log(a,b)
+
+// let day = 0
+
+// HAFTA KUNLARI 
+
+
+// switch(day) {
+//     case 1 :
+//         console.log("Dushanba")
+//         break
+//     case 2 :
+//         console.log("Seshanba")
+//         break
+//     case 3 :
+//         console.log("Chorshanba")
+//         break
+//     case 4 :
+//         console.log("Payshanba")
+//         break
+//     case 5 :
+//         console.log("Juma")
+//         break
+//     case 6 :
+//         console.log("Shanba")
+//         break
+//     case 7 :
+//         console.log("Yakshanba")
+//         break
+//     default :
+//         console.log("Bunday hafta  yo'q")
+
+// }
+
+//  MINI KALKULATOR
+
+// let son1 = 4, son2 = 5 , operator = "/"
+
+// switch(operator){
+//     case "-" : 
+//         console.log(son1 - son2)
+//         break
+//     case "+" : 
+//         console.log(son1 + son2)
+//         break
+//     case "/" : 
+//         console.log(son1 / son2)
+//         break
+//     case "*" : 
+//         console.log(son1 * son2)
+//         break
+//     default :
+//         console.log("Bunday amalni bajarib bo'lmaydi")
+// }
+
+// let talaba = {
+//     yosh : "20",
+//     ismi : "Asadbek",
+//     kursi : "2"
+// }
+
+// yosh = talaba.yosh
+// ismi = talaba.ismi
+// kursi = talaba.kursi
+
+
+
+// console.log(`Yoshi : ${yosh},`, `Ismi : ${ismi},`,`Kursi : ${kursi}`)
+
+let month = "7"
+
+
+let Qish = {
+    oy12 : "Dekabr",
+    oy1 : "yanvar",
+    oy2 : "fevral",
+}
+let Bahor = {
+    oy3 : "Mart",
+    oy4 : "Aprel",
+    oy5 : "may",
+}
+let Yoz = {
+    oy6 : "Iyun",
+    oy7 : "Iyul",
+    oy8 : "Avgust",
+}
+let Kuz = {
+    oy9 : "Sentabr",
+    oy10 : "Oktabr",
+    oy11 : "Noyabr",
+}
+
+let a
+
+switch (month) {
+    case "1" :
+        a = Qish.oy1
+        console.log(`${a} - Qish fasli`)
+        break
+    case "2" :
+        a = Qish.oy2
+        console.log(`${a} - Qish fasli`)
+        break
+    case "3" :
+        a = Bahor.oy3
+        console.log(`${a} - Bahor fasli`)
+        break
+    case "4" :
+        a = Bahor.oy4
+        console.log(`${a} - Bahor fasli`)
+        break
+    case "5" :
+        a = Bahor.oy5
+        console.log(`${a} - Bahor fasli`)
+        break
+    case "6" :
+        a = Yoz.oy6
+        console.log(`${a} - Yoz fasli`)
+        break
+    case "7" :
+        a = Yoz.oy7
+        console.log(`${a} - Yoz fasli`)
+        break
+    case "8" :
+        a = Yoz.oy8
+        console.log(`${a} - Yoz fasli`)
+        break
+    case "9" :
+        a = Kuz.oy9
+        console.log(`${a} - Kuz fasli`)
+        break
+    case "10" :
+        a = Kuz.oy10
+        console.log(`${a} - Kuz fasli`)
+        break
+    case "11" :
+        a = Kuz.oy11
+        console.log(`${a} - Kuz fasli`)
+        break
+    case "12" :
+        a = Qish.oy12
+        console.log(`${a} - Qish fasli`)
+        break
+    default :
+        console.log("Bunday oy mavjud emas")
+}
